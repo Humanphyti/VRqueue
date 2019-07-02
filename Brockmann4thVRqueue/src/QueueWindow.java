48cf2c865ee5a3dbf79cd40f3f7e9923d2ff322b
Humanphyti <humanphyti@gmail.com>
2019-07-01 18:59:09 -0700

    Initial commit
    UI layout generally finished save for the time implementation.

--- Brockmann4thVRqueue/src/QueueWindow.java
+++ Brockmann4thVRqueue/src/QueueWindow.java
@@ -1,13 +1,21 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.Timer;
 
 public class QueueWindow extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -30,11 +38,80 @@ public class QueueWindow extends JFrame {
 	 */
 	public QueueWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1920, 1080);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 31, 30, 30, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblOnDeck = new JLabel("On Deck");
+		GridBagConstraints gbc_lblOnDeck = new GridBagConstraints();
+		gbc_lblOnDeck.gridwidth = 12;
+		gbc_lblOnDeck.insets = new Insets(0, 0, 5, 5);
+		gbc_lblOnDeck.gridx = 0;
+		gbc_lblOnDeck.gridy = 1;
+		contentPane.add(lblOnDeck, gbc_lblOnDeck);
+		
+		JTextPane textPane = new JTextPane();
+		GridBagConstraints gbc_textPane = new GridBagConstraints();
+		gbc_textPane.gridwidth = 12;
+		gbc_textPane.gridheight = 32;
+		gbc_textPane.insets = new Insets(0, 0, 5, 5);
+		gbc_textPane.fill = GridBagConstraints.BOTH;
+		gbc_textPane.gridx = 0;
+		gbc_textPane.gridy = 2;
+		contentPane.add(textPane, gbc_textPane);
+		
+		JLabel lblPlayer = new JLabel("Player");
+		GridBagConstraints gbc_lblPlayer = new GridBagConstraints();
+		gbc_lblPlayer.gridwidth = 7;
+		gbc_lblPlayer.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPlayer.gridx = 13;
+		gbc_lblPlayer.gridy = 3;
+		contentPane.add(lblPlayer, gbc_lblPlayer);
+		
+		JLabel lblEnterName = new JLabel("Enter Name");
+		GridBagConstraints gbc_lblEnterName = new GridBagConstraints();
+		gbc_lblEnterName.gridwidth = 7;
+		gbc_lblEnterName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEnterName.gridx = 24;
+		gbc_lblEnterName.gridy = 3;
+		contentPane.add(lblEnterName, gbc_lblEnterName);
+		
+		JTextPane textPane_1 = new JTextPane();
+		GridBagConstraints gbc_textPane_1 = new GridBagConstraints();
+		gbc_textPane_1.gridwidth = 7;
+		gbc_textPane_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textPane_1.fill = GridBagConstraints.BOTH;
+		gbc_textPane_1.gridx = 13;
+		gbc_textPane_1.gridy = 4;
+		contentPane.add(textPane_1, gbc_textPane_1);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 8;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 24;
+		gbc_textField.gridy = 4;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JButton btnSubmit = new JButton("Submit");
+		GridBagConstraints gbc_btnSubmit = new GridBagConstraints();
+		gbc_btnSubmit.fill = GridBagConstraints.BOTH;
+		gbc_btnSubmit.gridwidth = 3;
+		gbc_btnSubmit.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSubmit.gridx = 32;
+		gbc_btnSubmit.gridy = 4;
+		contentPane.add(btnSubmit, gbc_btnSubmit);
+		
+		
 	}
 
 }
