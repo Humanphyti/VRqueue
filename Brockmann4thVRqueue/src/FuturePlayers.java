cbf93b0bcda0fca75e3cc3a94e545bb8e49f51e4
Humanphyti <humanphyti@gmail.com>
2019-07-03 05:06:51 -0700

    App Completed and bugs Squashed

--- Brockmann4thVRqueue/src/FuturePlayers.java
+++ Brockmann4thVRqueue/src/FuturePlayers.java
@@ -1,7 +1,8 @@
 
 public class FuturePlayers extends Players{
 	
-	public FuturePlayers(String Name) {
+	public FuturePlayers(String Name, String Game) {
 		this.name = Name;
+		this.game = Game;
 	}
 }
